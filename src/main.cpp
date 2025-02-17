2402695f922d93c2b26b6967b8ec8c51fe11b34a
Brendan Kirkpatrick <kirkpabd@mail.uc.edu>
2025-02-17 14:25:59 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "test" << std::endl;
+
+  int x = 0;
+
+  return x;
+}
---
+++ test/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "test fn" << std::endl;
+    return 0;
+}
